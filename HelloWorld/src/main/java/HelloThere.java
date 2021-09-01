50b3f7fe124189535df7b85f7a4e749b4a656e1b
DanBerkness <berknessdanielr@gmail.com>
2021-09-01 12:51:11 -0500

    new file:   .gitignore A file like this has been committed, and 	will be pushed. 	new file:   HelloWorld/.classpath Here is another file. Here are some of the files that will be pushed in this scenerio.

---
+++ HelloWorld/src/main/java/HelloThere.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+public class HelloThere {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String hello = "Hello World";
+		System.out.println(hello);
+	}
+
+}
